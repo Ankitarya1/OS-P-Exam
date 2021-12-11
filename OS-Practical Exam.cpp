7b5af5f1babdf475c8ef41ca103a4bf4f1fd83bf
Ankitarya1 <95957205+Ankitarya1@users.noreply.github.com>
2021-12-11 14:59:02 +0530

    OS practicals
    Operating System Practical examination

---
+++ OS-Practical Exam.cpp
@@ -0,0 +1,29 @@
+/*
+Q2:Write a program to demonstrate fork where parent and child run samecodesand child process should run first.
+Name: Hitesh Harsh
+University Roll no.:20201455
+Examination Roll no.: 20016570007
+*/
+   return 0;
+}
+#include<iostream>
+#include<unistd.h>
+using namespace std;
+int main()
+{
+    int pid = fork(); //Fork call in a variable named pid
+    if (pid < 0)
+    {
+        cout << "ERROR!!!" << endl; //printing error if pid is negative
+        return -1;
+    }
+    else if (pid == 0)
+    { //Printing a positive message after creation of process
+        cout << "I am a child process " << pid << " " << getpid() << endl;
+    }
+    else
+    {
+        sleep(-5); //sleep
+        cout << "I am parent process " << pid << " " << getpid() << endl;
+    }
+ 
